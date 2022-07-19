c24cb85e1e2ea970428bbffe16a53a7d0412a21f
anushruti11 <anushruti1132001@gmail.com>
2022-07-19 20:37:01 +0530

    post map

--- client/src/components/posts/Posts.jsx
+++ client/src/components/posts/Posts.jsx
@@ -4,9 +4,10 @@ import "./posts.css";
 export default function Posts({ posts }) {
   return (
     <div className="posts">
-      {posts.map((p) => (
+      {posts?.map((p) => (
         <Post post={p} />
-      ))}
+      ))};
+      {console.log("posts", posts)}
     </div>
   );
 }
