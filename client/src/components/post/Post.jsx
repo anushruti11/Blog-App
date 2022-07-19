9758f89bfb3b0c3beb7d09a63123423129a5f56d
anushruti11 <anushruti1132001@gmail.com>
2022-07-19 14:20:34 +0530

    bug-fix

--- client/src/components/post/Post.jsx
+++ client/src/components/post/Post.jsx
@@ -7,11 +7,11 @@ export default function Post({ post }) {
     <div className="post">
       {post.photo && <img className="postImg" src={PF + post.photo} alt="" />}
       <div className="postInfo">
-        <div className="postCats">
-          {post.categories.map((c) => (
+        {/* <div className="postCats">
+          {post.categories.((c) => (
             <span className="postCat">{c.name}</span>
           ))}
-        </div>
+        </div> */}
         <Link to={`/post/${post._id}`} className="link">
           <span className="postTitle">{post.title}</span>
         </Link>
